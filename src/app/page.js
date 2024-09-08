128f11ba67c0769554dcebb9bec398a299b429c1
RoziqovM <mroziqov494@gmail.com>
2024-09-08 14:44:17 +0500

    nextjs tayyorlangan fayl olib ishlatishga

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-slate-700 flex items-center justify-center">
+      <h1 className="font-bold text-[64px]  text-yellow-800 underline">
+        Hi Muhammadali
+      </h1>
+    </div>
+  );
+}
